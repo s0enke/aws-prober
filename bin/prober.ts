7a486381e70d628926a27b6e75b0f0e007f75444
Soenke Ruempler <soenke@ruempler.eu>
2023-01-12 14:10:00 +0100

    chore: prepare for deployment into fixed bucket

--- bin/prober.ts
+++ bin/prober.ts
@@ -2,9 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProberStack } from '../lib/prober-stack';
+import {CliCredentialsStackSynthesizer} from "aws-cdk-lib";
 
 const app = new cdk.App();
 new ProberStack(app, 'ProberStack', {
+  // synthesizer: new CliCredentialsStackSynthesizer({
+  //   fileAssetsBucketName: 'prober-assets-${AWS::Region}',
+  // }),
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
