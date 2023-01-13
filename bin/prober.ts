1959c18d867602eaeae2dcc290dfcb84ec3d68d4
Soenke Ruempler <soenke@ruempler.eu>
2023-01-13 11:51:13 +0100

    chore: prettier

--- bin/prober.ts
+++ bin/prober.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { ProberStack } from '../lib/prober-stack';
-import {CliCredentialsStackSynthesizer} from "aws-cdk-lib";
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { ProberStack } from "../lib/prober-stack";
+import { CliCredentialsStackSynthesizer } from "aws-cdk-lib";
 import * as process from "process";
 
 const app = new cdk.App();
-new ProberStack(app, 'ProberStack', {
+new ProberStack(app, "ProberStack", {
   synthesizer: new CliCredentialsStackSynthesizer({
     fileAssetsBucketName: process.env.FILE_ASSETS_BUCKET_NAME ?? undefined,
     bucketPrefix: process.env.FILE_ASSETS_BUCKET_PREFIX ?? undefined,
--- lib/prober-stack.ts
+++ lib/prober-stack.ts
@@ -1,113 +1,151 @@
-import * as cdk from 'aws-cdk-lib';
-import * as config from 'aws-cdk-lib/aws-config';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as cr from 'aws-cdk-lib/custom-resources';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
-import * as lambda_python from '@aws-cdk/aws-lambda-python-alpha';
-import {Aws} from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
+import * as config from "aws-cdk-lib/aws-config";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as cr from "aws-cdk-lib/custom-resources";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
+import * as lambda_python from "@aws-cdk/aws-lambda-python-alpha";
+import { Aws } from "aws-cdk-lib";
 
 export class ProberStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const serviceLinkedRoleForAwsConfigIfNotExists = new cr.AwsCustomResource(this, 'ServiceLinkedRoleForAwsConfigIfNotExists', {
-      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
-        resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE,
-      }),
-      onCreate: {
-        action: 'createServiceLinkedRole',
-        service: 'IAM',
-        physicalResourceId: cr.PhysicalResourceId.of('ServiceLinkedRoleForAwsConfig'),
-        parameters: {
-          AWSServiceName: 'config.amazonaws.com',
-        },
-        ignoreErrorCodesMatching: 'InvalidInput', // ignore if already created
-      },
-    });
-
-    // AWS config rules need a recorder to work, so create a dummy recorder with minimum costs if none exists
-    const configRecorderIfNotExists = new cr.AwsCustomResource(this, 'ConfigRecorderIfNotExists', {
-      policy: cr.AwsCustomResourcePolicy.fromStatements(
-        [
-          new iam.PolicyStatement({
-            actions: ['iam:PassRole', 'config:PutConfigurationRecorder', 'config:DeleteConfigurationRecorder'],
-            resources: ['*'],
-            effect: iam.Effect.ALLOW
-          })
-        ],
-      ),
-      onCreate: {
-        action: 'putConfigurationRecorder',
-        service: 'ConfigService',
-        physicalResourceId: cr.PhysicalResourceId.of('ConfigRecorderIfNotExists'),
-        parameters: {
-          ConfigurationRecorder: {
-            name: 'prober',
-            recordingGroup: {
-              allSupported: false,
-              includeGlobalResourceTypes: false,
-              resourceTypes: [
-                'Datadog::SLOs::SLO', // some dummy resource type nobody uses
-              ],
-            },
-            roleARN: `arn:aws:iam::${Aws.ACCOUNT_ID}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig`,
-          }
-        },
-        ignoreErrorCodesMatching: 'MaxNumberOfConfigurationRecordersExceeded', // ignore if already created
-      },
-      onDelete: {
-        action: 'deleteConfigurationRecorder',
-        service: 'ConfigService',
-        physicalResourceId: cr.PhysicalResourceId.of('ConfigRecorderIfNotExists'),
-        parameters: {
-          ConfigurationRecorderName: 'prober',
+    const serviceLinkedRoleForAwsConfigIfNotExists = new cr.AwsCustomResource(
+      this,
+      "ServiceLinkedRoleForAwsConfigIfNotExists",
+      {
+        policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+          resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+        }),
+        onCreate: {
+          action: "createServiceLinkedRole",
+          service: "IAM",
+          physicalResourceId: cr.PhysicalResourceId.of(
+            "ServiceLinkedRoleForAwsConfig"
+          ),
+          parameters: {
+            AWSServiceName: "config.amazonaws.com",
+          },
+          ignoreErrorCodesMatching: "InvalidInput", // ignore if already created
         },
       }
-    });
-    configRecorderIfNotExists.node.addDependency(serviceLinkedRoleForAwsConfigIfNotExists);
+    );
 
+    // AWS config rules need a recorder to work, so create a dummy recorder with minimum costs if none exists
+    const configRecorderIfNotExists = new cr.AwsCustomResource(
+      this,
+      "ConfigRecorderIfNotExists",
+      {
+        policy: cr.AwsCustomResourcePolicy.fromStatements([
+          new iam.PolicyStatement({
+            actions: [
+              "iam:PassRole",
+              "config:PutConfigurationRecorder",
+              "config:DeleteConfigurationRecorder",
+            ],
+            resources: ["*"],
+            effect: iam.Effect.ALLOW,
+          }),
+        ]),
+        onCreate: {
+          action: "putConfigurationRecorder",
+          service: "ConfigService",
+          physicalResourceId: cr.PhysicalResourceId.of(
+            "ConfigRecorderIfNotExists"
+          ),
+          parameters: {
+            ConfigurationRecorder: {
+              name: "prober",
+              recordingGroup: {
+                allSupported: false,
+                includeGlobalResourceTypes: false,
+                resourceTypes: [
+                  "Datadog::SLOs::SLO", // some dummy resource type nobody uses
+                ],
+              },
+              roleARN: `arn:aws:iam::${Aws.ACCOUNT_ID}:role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig`,
+            },
+          },
+          ignoreErrorCodesMatching: "MaxNumberOfConfigurationRecordersExceeded", // ignore if already created
+        },
+        onDelete: {
+          action: "deleteConfigurationRecorder",
+          service: "ConfigService",
+          physicalResourceId: cr.PhysicalResourceId.of(
+            "ConfigRecorderIfNotExists"
+          ),
+          parameters: {
+            ConfigurationRecorderName: "prober",
+          },
+        },
+      }
+    );
+    configRecorderIfNotExists.node.addDependency(
+      serviceLinkedRoleForAwsConfigIfNotExists
+    );
 
-    new config.ManagedRule(this, 'prober-security-root-account-mfa-enabled', {
-      configRuleName: 'prober-security-root-account-mfa-enabled',
-      identifier: 'ROOT_ACCOUNT_MFA_ENABLED',
+    new config.ManagedRule(this, "prober-security-root-account-mfa-enabled", {
+      configRuleName: "prober-security-root-account-mfa-enabled",
+      identifier: "ROOT_ACCOUNT_MFA_ENABLED",
     }).node.addDependency(configRecorderIfNotExists);
 
-    new config.ManagedRule(this, 'prober-security-iam-root-access-key-check', {
-      configRuleName: 'prober-security-iam-root-access-key-check',
-      identifier: 'IAM_ROOT_ACCESS_KEY_CHECK',
+    new config.ManagedRule(this, "prober-security-iam-root-access-key-check", {
+      configRuleName: "prober-security-iam-root-access-key-check",
+      identifier: "IAM_ROOT_ACCESS_KEY_CHECK",
     }).node.addDependency(configRecorderIfNotExists);
 
-    const proberFunction =  new lambda_python.PythonFunction(this, 'ProberFunction', {
-      entry: 'proberfunction/',
-      runtime: lambda.Runtime.PYTHON_3_9,
-      timeout: cdk.Duration.seconds(60),
-    });
-    proberFunction.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['compute-optimizer:GetEnrollmentStatus', 'organizations:DescribeAccount', 'organizations:DescribeOrganization', 'iam:ListUsers', 'budgets:ViewBudget', 'ce:GetAnomalyMonitors'],
-      resources: ['*'],
-      effect: iam.Effect.ALLOW
+    const proberFunction = new lambda_python.PythonFunction(
+      this,
+      "ProberFunction",
+      {
+        entry: "proberfunction/",
+        runtime: lambda.Runtime.PYTHON_3_9,
+        timeout: cdk.Duration.seconds(60),
+      }
+    );
+    proberFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "compute-optimizer:GetEnrollmentStatus",
+          "organizations:DescribeAccount",
+          "organizations:DescribeOrganization",
+          "iam:ListUsers",
+          "budgets:ViewBudget",
+          "ce:GetAnomalyMonitors",
+        ],
+        resources: ["*"],
+        effect: iam.Effect.ALLOW,
       })
     );
 
-    const awsApiLibRole = new iam.Role(this, 'AwsApiLibRole', {
+    const awsApiLibRole = new iam.Role(this, "AwsApiLibRole", {
       assumedBy: new iam.ArnPrincipal(proberFunction.role?.roleArn!),
       inlinePolicies: {
-        'account': new iam.PolicyDocument({
+        account: new iam.PolicyDocument({
           statements: [
             new iam.PolicyStatement({
-              actions: ['*'], // FIXME: least privilege
-              resources: ['*'],
-              effect: iam.Effect.ALLOW
-            })
-          ]
-        })
-      }
+              actions: ["*"], // FIXME: least privilege
+              resources: ["*"],
+              effect: iam.Effect.ALLOW,
+            }),
+          ],
+        }),
+      },
     });
-    proberFunction.addEnvironment('AWS_API_LIB_ROLE', awsApiLibRole.roleArn);
+    proberFunction.addEnvironment("AWS_API_LIB_ROLE", awsApiLibRole.roleArn);
 
-    for (let probeName of ['billing-invoice-by-email-enabled', 'billing-compute-optimizer-enabled', 'billing-iam-access-enabled', 'billing-tax-inheritance-enabled', 'billing-budget-created', 'billing-cost-anomaly-detector-created', 'security-account-is-organizations-management-account', 'security-account-has-no-iam-users']) {
+    for (let probeName of [
+      "billing-invoice-by-email-enabled",
+      "billing-compute-optimizer-enabled",
+      "billing-iam-access-enabled",
+      "billing-tax-inheritance-enabled",
+      "billing-budget-created",
+      "billing-cost-anomaly-detector-created",
+      "security-account-is-organizations-management-account",
+      "security-account-has-no-iam-users",
+    ]) {
       new config.CustomRule(this, `prober-${probeName}`, {
         configRuleName: `prober-${probeName}`,
         inputParameters: {
@@ -118,27 +156,31 @@ export class ProberStack extends cdk.Stack {
       }).node.addDependency(configRecorderIfNotExists);
     }
 
-
-    const proberDashboard = new cloudwatch.Dashboard(this, 'proberDashboard', {
-      dashboardName: "aws-prober"
+    const proberDashboard = new cloudwatch.Dashboard(this, "proberDashboard", {
+      dashboardName: "aws-prober",
     });
 
-    const proberDashboardFunction =  new lambda_python.PythonFunction(this, 'proberDashboardFunction', {
-      entry: 'dashboard/',
-      runtime: lambda.Runtime.PYTHON_3_9,
-      timeout: cdk.Duration.seconds(60),
-    });
-    proberDashboardFunction.addToRolePolicy(new iam.PolicyStatement({
-        actions: ['config:Describe*', 'config:StartConfigRulesEvaluation'],
-        resources: ['*'],
-        effect: iam.Effect.ALLOW
+    const proberDashboardFunction = new lambda_python.PythonFunction(
+      this,
+      "proberDashboardFunction",
+      {
+        entry: "dashboard/",
+        runtime: lambda.Runtime.PYTHON_3_9,
+        timeout: cdk.Duration.seconds(60),
+      }
+    );
+    proberDashboardFunction.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["config:Describe*", "config:StartConfigRulesEvaluation"],
+        resources: ["*"],
+        effect: iam.Effect.ALLOW,
       })
     );
 
     // Create a custom widget for the dashboard
     const customWidget = new cloudwatch.CustomWidget({
       functionArn: proberDashboardFunction.functionArn,
-      title: '',
+      title: "",
       width: 24,
       height: 40,
     });
